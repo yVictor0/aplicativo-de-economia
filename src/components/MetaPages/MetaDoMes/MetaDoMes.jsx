b45afb10f40f82c509ba384cbe2a0f086e3ab585
Yan Victor <yanvictormt@gmail.com>
2026-09-23 22:56:35 -0300

    arrumando porcetagem

--- src/components/MetaPages/MetaDoMes/MetaDoMes.jsx
+++ src/components/MetaPages/MetaDoMes/MetaDoMes.jsx
@@ -1,8 +1,8 @@
 import styles from "./MetaDoMes.module.css";
 
-const valor = "1.500,00"; 
 
-function MetaDoMes({atual = 60, maximo = 1500}) {
+
+function MetaDoMes({atual = 1400, maximo = 2000}) {
 
     const porcentagem = maximo > 0 
     ? Math.min(Math.max((atual / maximo ) * 100, 0), 100) 
@@ -13,8 +13,8 @@ function MetaDoMes({atual = 60, maximo = 1500}) {
         <div>
             <div className={styles.MetaDoMes}>
                 <div className={styles.icon}></div>
-                <p className={styles.titleMetaDoMes}> Meta Do Mes </p>
-                <h1 className={styles.valor} >{valor}</h1>
+                <p className={styles.titleMetaDoMes}> Meta Do Mes {maximo} </p>
+                <h1 className={styles.valor} >{maximo}</h1>
                 <h1 className={styles.porcentagem}> {porcentagem.toFixed(0)}% </h1>
 
             </div>
